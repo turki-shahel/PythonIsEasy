7c15218ec7475a1d51fe23a604d1756a30ebd56a
Turki Shahel <tshahel@gmail.com>
2019-01-13 21:11:48 +0300

    final

--- homework#1/main.py
+++ homework#1/main.py
@@ -2,29 +2,29 @@
     Homework #1 by Turki Shahel
     Listing all song attributes by using variables'''
 
-TrackNumber = 1
-Title       = 'On my way'
-Artist      = 'Turki Shahel'
-DurationInSeconds = 6000
-AlbumName   = 'Python Is Easy'
-ReleaseYear = 2019
-Genre       = 'Alternative'
-Studio      = 'Pirple'
-Composer    = 'Max'
+TrackNumber= 1
+Title= 'On my way'
+Artist= 'Turki Shahel'
+DurationInSeconds= 6000
+AlbumName= 'Python Is Easy'
+ReleaseYear= 2019
+Genre= 'Alternative'
+Studio= 'Pirple'
+Composer= 'Max'
 
-print('#'*20)
-print('#')
+print('#'*22)
+print('#                    #')
 print('#    Homework #1     #')
 print('#        by          #')
 print('#    Turki Shahel    #')
-print('#'*20)
+print('#'*22)
 print('Song Attributes')
-print('('+TrackNumber + ')')
+print('Track number('+str(TrackNumber) + ')')
 print('|Title: ' + Title)
 print('|Artitst: ' + Artist)
-print('|Duration In Seconds: ' + DurationInSeconds)
+print('|Duration In Seconds: ' + str(DurationInSeconds))
 print('|Album Name: ' + AlbumName)
-print('|Release Year: ' + ReleaseYear)
+print('|Release Year: ' + str(ReleaseYear))
 print('|Genre: ' + Genre)
 print('|Studio: ' + Studio)
 print('|Composer: ' + Composer)
