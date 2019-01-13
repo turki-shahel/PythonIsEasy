ce3a0e8cff685a0193da41e90548e46dcca6c072
Turki Shahel <tshahel@gmail.com>
2019-01-13 20:59:51 +0300

    Homework#1Dir

---
+++ homework#1/main.py
@@ -0,0 +1,31 @@
+'''Python Is Easy course by Pirple
+    Homework #1 by Turki Shahel
+    Listing all song attributes by using variables'''
+
+TrackNumber = 1
+Title       = 'On my way'
+Artist      = 'Turki Shahel'
+DurationInSeconds = 6000
+AlbumName   = 'Python Is Easy'
+ReleaseYear = 2019
+Genre       = 'Alternative'
+Studio      = 'Pirple'
+Composer    = 'Max'
+
+print('#'*20)
+print('#')
+print('#    Homework #1     #')
+print('#        by          #')
+print('#    Turki Shahel    #')
+print('#'*20)
+print('Song Attributes')
+print('('+TrackNumber + ')')
+print('|Title: ' + Title)
+print('|Artitst: ' + Artist)
+print('|Duration In Seconds: ' + DurationInSeconds)
+print('|Album Name: ' + AlbumName)
+print('|Release Year: ' + ReleaseYear)
+print('|Genre: ' + Genre)
+print('|Studio: ' + Studio)
+print('|Composer: ' + Composer)
+
